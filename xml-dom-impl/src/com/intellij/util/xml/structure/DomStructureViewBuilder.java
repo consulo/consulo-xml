9e22282e83ac78884cd3b12222cb725c518a508e
VISTALL <vistall.valeriy@gmail.com>
2014-02-08 23:20:54 +0200

    to prev

--- xml-dom-impl/src/com/intellij/util/xml/structure/DomStructureViewBuilder.java
+++ xml-dom-impl/src/com/intellij/util/xml/structure/DomStructureViewBuilder.java
@@ -16,64 +16,81 @@
 
 package com.intellij.util.xml.structure;
 
+import org.jetbrains.annotations.NotNull;
 import com.intellij.ide.structureView.StructureView;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.ide.structureView.newStructureView.StructureViewComponent;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.AsyncResult;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlElement;
-import com.intellij.psi.xml.XmlTag;
 import com.intellij.psi.xml.XmlFile;
+import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.Function;
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.DomManager;
 import com.intellij.util.xml.DomService;
-import org.jetbrains.annotations.NotNull;
 
-public class DomStructureViewBuilder extends TreeBasedStructureViewBuilder {
-  private final Function<DomElement, DomService.StructureViewMode> myDescriptor;
-  private final XmlFile myFile;
+public class DomStructureViewBuilder extends TreeBasedStructureViewBuilder
+{
+	private final Function<DomElement, DomService.StructureViewMode> myDescriptor;
+	private final XmlFile myFile;
 
-  public DomStructureViewBuilder(final XmlFile file, final Function<DomElement,DomService.StructureViewMode> descriptor) {
-    myFile = file;
-    myDescriptor = descriptor;
-  }
+	public DomStructureViewBuilder(final XmlFile file, final Function<DomElement, DomService.StructureViewMode> descriptor)
+	{
+		myFile = file;
+		myDescriptor = descriptor;
+	}
 
-  @NotNull
-  public StructureViewModel createStructureViewModel() {
-    return new DomStructureViewTreeModel(myFile, myDescriptor);
-  }
+	@Override
+	@NotNull
+	public StructureViewModel createStructureViewModel(Editor editor)
+	{
+		return new DomStructureViewTreeModel(myFile, myDescriptor);
+	}
 
-  public boolean isRootNodeShown() {
-    return true;
-  }
+	@Override
+	public boolean isRootNodeShown()
+	{
+		return true;
+	}
 
-  @NotNull
-  public StructureView createStructureView(final FileEditor fileEditor, final Project project) {
-    return new StructureViewComponent(fileEditor, createStructureViewModel(), project) {
-      public AsyncResult<AbstractTreeNode> expandPathToElement(final Object element) {
-        if (element instanceof XmlElement) {
-          final XmlElement xmlElement = (XmlElement)element;
-          XmlTag tag = PsiTreeUtil.getParentOfType(xmlElement, XmlTag.class, false);
-          while (tag != null) {
-            final DomElement domElement = DomManager.getDomManager(xmlElement.getProject()).getDomElement(tag);
-            if (domElement != null) {
-              for (DomElement curElement = domElement; curElement != null; curElement = curElement.getParent()) {
-                if (myDescriptor.fun(curElement) == DomService.StructureViewMode.SHOW) {
-                  return super.expandPathToElement(curElement.getXmlElement());
-                }
-              }
-            }
-            tag = PsiTreeUtil.getParentOfType(tag, XmlTag.class, true);
-          }
+	@Override
+	@NotNull
+	public StructureView createStructureView(final FileEditor fileEditor, final Project project)
+	{
+		return new StructureViewComponent(fileEditor, createStructureViewModel(), project)
+		{
+			@Override
+			public AsyncResult<AbstractTreeNode> expandPathToElement(final Object element)
+			{
+				if(element instanceof XmlElement)
+				{
+					final XmlElement xmlElement = (XmlElement) element;
+					XmlTag tag = PsiTreeUtil.getParentOfType(xmlElement, XmlTag.class, false);
+					while(tag != null)
+					{
+						final DomElement domElement = DomManager.getDomManager(xmlElement.getProject()).getDomElement(tag);
+						if(domElement != null)
+						{
+							for(DomElement curElement = domElement; curElement != null; curElement = curElement.getParent())
+							{
+								if(myDescriptor.fun(curElement) == DomService.StructureViewMode.SHOW)
+								{
+									return super.expandPathToElement(curElement.getXmlElement());
+								}
+							}
+						}
+						tag = PsiTreeUtil.getParentOfType(tag, XmlTag.class, true);
+					}
 
-        }
-        return super.expandPathToElement(element);
-      }
-    };
-  }
+				}
+				return super.expandPathToElement(element);
+			}
+		};
+	}
 }
