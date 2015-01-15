eaebfcd61dd9ca216b0b09535d72cb418e6530dd
VISTALL <vistall.valeriy@gmail.com>
2015-01-15 16:51:27 +0300

    compilation

--- xml-dom-impl/src/com/intellij/util/xml/highlighting/DomElementsErrorPanel.java
+++ xml-dom-impl/src/com/intellij/util/xml/highlighting/DomElementsErrorPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,14 @@
 
 package com.intellij.util.xml.highlighting;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+import org.jetbrains.annotations.NotNull;
 import com.intellij.codeInsight.daemon.impl.SeverityRegistrar;
 import com.intellij.codeInsight.daemon.impl.TrafficLightRenderer;
 import com.intellij.icons.AllIcons;
@@ -25,7 +33,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiFile;
+import com.intellij.psi.xml.XmlFile;
 import com.intellij.util.Alarm;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.xml.DomChangeAdapter;
@@ -35,164 +43,212 @@ import com.intellij.util.xml.DomUtil;
 import com.intellij.util.xml.ui.CommittablePanel;
 import com.intellij.util.xml.ui.Highlightable;
 
-import javax.swing.*;
-import java.awt.*;
-
 /**
  * User: Sergey.Vasiliev
  */
-public class DomElementsErrorPanel extends JPanel implements CommittablePanel, Highlightable {
+public class DomElementsErrorPanel extends JPanel implements CommittablePanel, Highlightable
+{
 
-  private static final int ALARM_PERIOD = 241;
+	private static final int ALARM_PERIOD = 241;
 
-  private final Project myProject;
-  private final DomElement[] myDomElements;
+	private final Project myProject;
+	private final DomElement[] myDomElements;
 
-  private final DomElementsTrafficLightRenderer myErrorStripeRenderer;
-  private final DomElementAnnotationsManagerImpl myAnnotationsManager;
+	private final DomElementsTrafficLightRenderer myErrorStripeRenderer;
+	private final DomElementAnnotationsManagerImpl myAnnotationsManager;
 
-  private final Alarm myAlarm = new Alarm();
+	private final Alarm myAlarm = new Alarm();
 
-  public DomElementsErrorPanel(final DomElement... domElements) {
-    assert domElements.length > 0;
+	public DomElementsErrorPanel(final DomElement... domElements)
+	{
+		assert domElements.length > 0;
 
-    myDomElements = domElements;
-    final DomManager domManager = domElements[0].getManager();
-    myProject = domManager.getProject();
-    myAnnotationsManager = (DomElementAnnotationsManagerImpl)DomElementAnnotationsManager.getInstance(myProject);
+		myDomElements = domElements;
+		final DomManager domManager = domElements[0].getManager();
+		myProject = domManager.getProject();
+		myAnnotationsManager = (DomElementAnnotationsManagerImpl) DomElementAnnotationsManager.getInstance(myProject);
 
-    setPreferredSize(getDimension());
+		setPreferredSize(getDimension());
 
-    myErrorStripeRenderer = new DomElementsTrafficLightRenderer(DomUtil.getFile(domElements[0]));
-    Disposer.register(this, myErrorStripeRenderer);
+		myErrorStripeRenderer = new DomElementsTrafficLightRenderer(DomUtil.getFile(domElements[0]));
+		Disposer.register(this, myErrorStripeRenderer);
 
-    addUpdateRequest();
-    domManager.addDomEventListener(new DomChangeAdapter() {
-      protected void elementChanged(DomElement element) {
-        addUpdateRequest();
-      }
-    }, this);
-  }
+		addUpdateRequest();
+		domManager.addDomEventListener(new DomChangeAdapter()
+		{
+			@Override
+			protected void elementChanged(DomElement element)
+			{
+				addUpdateRequest();
+			}
+		}, this);
+	}
 
-  public void updateHighlighting() {
-    updatePanel();
-  }
+	@Override
+	public void updateHighlighting()
+	{
+		updatePanel();
+	}
 
-  private boolean areValid() {
-    for (final DomElement domElement : myDomElements) {
-      if (!domElement.isValid()) return false;
-    }
-    return true;
-  }
+	private boolean areValid()
+	{
+		for(final DomElement domElement : myDomElements)
+		{
+			if(!domElement.isValid())
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 
-  private void updatePanel() {
-    myAlarm.cancelAllRequests();
+	private void updatePanel()
+	{
+		myAlarm.cancelAllRequests();
 
-    if (!areValid()) return;
+		if(!areValid())
+		{
+			return;
+		}
 
-    repaint();
-    setToolTipText(myErrorStripeRenderer.getTooltipMessage());
+		repaint();
+		setToolTipText(myErrorStripeRenderer.getTooltipMessage());
 
-    if (!isHighlightingFinished()) {
-      addUpdateRequest();
-    }
-  }
+		if(!isHighlightingFinished())
+		{
+			addUpdateRequest();
+		}
+	}
 
-  private boolean isHighlightingFinished() {
-    return !areValid() || myAnnotationsManager.isHighlightingFinished(myDomElements);
-  }
+	private boolean isHighlightingFinished()
+	{
+		return !areValid() || myAnnotationsManager.isHighlightingFinished(myDomElements);
+	}
 
-  private void addUpdateRequest() {
-    ApplicationManager.getApplication().invokeLater(new Runnable() {
+	private void addUpdateRequest()
+	{
+		ApplicationManager.getApplication().invokeLater(new Runnable()
+		{
 
-      public void run() {
-        myAlarm.addRequest(new Runnable() {
-          public void run() {
-            if (myProject.isOpen() && !myProject.isDisposed()) {
-              updatePanel();
-            }
-          }
-        }, ALARM_PERIOD);
-      }
-    });
-  }
+			@Override
+			public void run()
+			{
+				myAlarm.addRequest(new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						if(myProject.isOpen() && !myProject.isDisposed())
+						{
+							updatePanel();
+						}
+					}
+				}, ALARM_PERIOD);
+			}
+		});
+	}
 
-  protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
 
-    myErrorStripeRenderer.paint(this, g, new Rectangle(0, 0, getWidth(), getHeight()));
-  }
+		myErrorStripeRenderer.paint(this, g, new Rectangle(0, 0, getWidth(), getHeight()));
+	}
 
-  public void dispose() {
-    myAlarm.cancelAllRequests();
-  }
+	@Override
+	public void dispose()
+	{
+		myAlarm.cancelAllRequests();
+	}
 
-  public JComponent getComponent() {
-    return this;
-  }
+	@Override
+	public JComponent getComponent()
+	{
+		return this;
+	}
 
-  public void commit() {
-  }
+	@Override
+	public void commit()
+	{
+	}
 
-  public void reset() {
-    updatePanel();
-  }
+	@Override
+	public void reset()
+	{
+		updatePanel();
+	}
 
-  private static Dimension getDimension() {
-    return new Dimension(AllIcons.General.ErrorsInProgress.getIconWidth() + 2, AllIcons.General.ErrorsInProgress.getIconHeight() + 2);
-  }
+	private static Dimension getDimension()
+	{
+		return new Dimension(AllIcons.General.ErrorsInProgress.getIconWidth() + 2, AllIcons.General.ErrorsInProgress.getIconHeight() + 2);
+	}
 
-  private class DomElementsTrafficLightRenderer extends TrafficLightRenderer {
+	private class DomElementsTrafficLightRenderer extends TrafficLightRenderer
+	{
+		public DomElementsTrafficLightRenderer(@NotNull XmlFile xmlFile)
+		{
+			super(xmlFile.getProject(), PsiDocumentManager.getInstance(xmlFile.getProject()).getDocument(xmlFile), xmlFile);
+		}
 
-    public DomElementsTrafficLightRenderer(final PsiFile xmlFile) {
-      super(xmlFile.getProject(),
-            PsiDocumentManager.getInstance(xmlFile.getProject()).getDocument(xmlFile), xmlFile);
-    }
+		@NotNull
+		@Override
+		protected DaemonCodeAnalyzerStatus getDaemonCodeAnalyzerStatus(@NotNull SeverityRegistrar severityRegistrar)
+		{
+			final DaemonCodeAnalyzerStatus status = super.getDaemonCodeAnalyzerStatus(severityRegistrar);
+			if(isInspectionCompleted())
+			{
+				status.errorAnalyzingFinished = true;
+			}
+			return status;
+		}
 
-    protected DaemonCodeAnalyzerStatus getDaemonCodeAnalyzerStatus(boolean fillErrorsCount, SeverityRegistrar severityRegistrar) {
-      final DaemonCodeAnalyzerStatus status = super.getDaemonCodeAnalyzerStatus(fillErrorsCount, severityRegistrar);
-      if (status != null && isInspectionCompleted()) {
-        status.errorAnalyzingFinished = true;
-      }
-      return status;
-    }
+		@Override
+		protected void fillDaemonCodeAnalyzerErrorsStatus(@NotNull DaemonCodeAnalyzerStatus status, @NotNull SeverityRegistrar severityRegistrar)
+		{
+			for(int i = 0; i < status.errorCount.length; i++)
+			{
+				final HighlightSeverity minSeverity = severityRegistrar.getSeverityByIndex(i);
+				if(minSeverity == null)
+				{
+					continue;
+				}
 
-    @Override
-    protected void fillDaemonCodeAnalyzerErrorsStatus(DaemonCodeAnalyzerStatus status,
-                                                      boolean fillErrorsCount,
-                                                      SeverityRegistrar severityRegistrar) {
-      for (int i = 0; i < status.errorCount.length; i++) {
-        final HighlightSeverity minSeverity = severityRegistrar.getSeverityByIndex(i);
-        if (minSeverity == null) {
-          continue;
-        }
+				int sum = 0;
+				for(DomElement element : myDomElements)
+				{
+					final DomElementsProblemsHolder holder = myAnnotationsManager.getCachedProblemHolder(element);
+					sum += (SeverityRegistrar.getSeverityRegistrar(getProject()).compare(minSeverity, HighlightSeverity.WARNING) >= 0 ? holder
+							.getProblems(element, true, true) : holder.getProblems(element, true, minSeverity)).size();
+				}
+				status.errorCount[i] = sum;
+			}
+		}
 
-        int sum = 0;
-        for (DomElement element : myDomElements) {
-          final DomElementsProblemsHolder holder = myAnnotationsManager.getCachedProblemHolder(element);
-          sum += (SeverityRegistrar.getSeverityRegistrar(getProject()).compare(minSeverity, HighlightSeverity.WARNING) >= 0 ? holder
-            .getProblems(element, true, true) : holder.getProblems(element, true, minSeverity)).size();
-        }
-        status.errorCount[i] = sum;
-      }
-    }
+		protected boolean isInspectionCompleted()
+		{
+			return ContainerUtil.and(myDomElements, new Condition<DomElement>()
+			{
+				@Override
+				public boolean value(final DomElement element)
+				{
+					return myAnnotationsManager.getHighlightStatus(element) == DomHighlightStatus.INSPECTIONS_FINISHED;
+				}
+			});
+		}
 
-    protected boolean isInspectionCompleted() {
-      return ContainerUtil.and(myDomElements, new Condition<DomElement>() {
-        public boolean value(final DomElement element) {
-          return myAnnotationsManager.getHighlightStatus(element) == DomHighlightStatus.INSPECTIONS_FINISHED;
-        }
-      });
-    }
+		protected boolean isErrorAnalyzingFinished()
+		{
+			return ContainerUtil.and(myDomElements, new Condition<DomElement>()
+			{
+				@Override
+				public boolean value(final DomElement element)
+				{
+					return myAnnotationsManager.getHighlightStatus(element).compareTo(DomHighlightStatus.ANNOTATORS_FINISHED) >= 0;
+				}
+			});
+		}
 
-    protected boolean isErrorAnalyzingFinished() {
-      return ContainerUtil.and(myDomElements, new Condition<DomElement>() {
-        public boolean value(final DomElement element) {
-          return myAnnotationsManager.getHighlightStatus(element).compareTo(DomHighlightStatus.ANNOTATORS_FINISHED) >= 0;
-        }
-      });
-    }
-
-  }
+	}
 
 }
