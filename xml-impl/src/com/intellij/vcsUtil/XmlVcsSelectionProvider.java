d7f6792da8e0f7448717f7d0d6ab0d283d909ae3
VISTALL <vistall.valeriy@gmail.com>
2015-04-20 17:57:40 +0300

    compilation

--- xml-impl/src/com/intellij/vcsUtil/XmlVcsSelectionProvider.java
+++ xml-impl/src/com/intellij/vcsUtil/XmlVcsSelectionProvider.java
@@ -15,7 +15,9 @@
  */
 package com.intellij.vcsUtil;
 
-import com.intellij.codeInsight.TargetElementUtilBase;
+import org.mustbe.consulo.RequiredDispatchThread;
+import com.intellij.codeInsight.TargetElementUtil;
+import com.intellij.codeInsight.TargetElementUtilEx;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
@@ -26,45 +28,59 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.psi.xml.XmlText;
+import com.intellij.util.containers.ContainerUtil;
 
 /**
  * @author yole
  */
-public class XmlVcsSelectionProvider implements VcsSelectionProvider {
-  public VcsSelection getSelection(VcsContext context) {
-    final Editor editor = context.getEditor();
-      if (editor == null) return null;
-      PsiElement psiElement = TargetElementUtilBase.findTargetElement(editor, TargetElementUtilBase.ELEMENT_NAME_ACCEPTED);
-      if (psiElement == null || !psiElement.isValid()) {
-        return null;
-      }
+public class XmlVcsSelectionProvider implements VcsSelectionProvider
+{
+	@RequiredDispatchThread
+	public VcsSelection getSelection(VcsContext context)
+	{
+		final Editor editor = context.getEditor();
+		if(editor == null)
+		{
+			return null;
+		}
+		PsiElement psiElement = TargetElementUtil.findTargetElement(editor, ContainerUtil.newHashSet(TargetElementUtilEx.ELEMENT_NAME_ACCEPTED));
+		if(psiElement == null || !psiElement.isValid())
+		{
+			return null;
+		}
 
-      final String actionName;
+		final String actionName;
 
-      if (psiElement instanceof XmlTag) {
-        actionName = VcsBundle.message("action.name.show.history.for.tag");
-      }
-      else if (psiElement instanceof XmlText) {
-        actionName = VcsBundle.message("action.name.show.history.for.text");
-      }
-      else {
-        return null;
-      }
+		if(psiElement instanceof XmlTag)
+		{
+			actionName = VcsBundle.message("action.name.show.history.for.tag");
+		}
+		else if(psiElement instanceof XmlText)
+		{
+			actionName = VcsBundle.message("action.name.show.history.for.text");
+		}
+		else
+		{
+			return null;
+		}
 
-      TextRange textRange = psiElement.getTextRange();
-      if (textRange == null) {
-        return null;
-      }
+		TextRange textRange = psiElement.getTextRange();
+		if(textRange == null)
+		{
+			return null;
+		}
 
-      VirtualFile virtualFile = psiElement.getContainingFile().getVirtualFile();
-      if (virtualFile == null) {
-        return null;
-      }
-      if (!virtualFile.isValid()) {
-        return null;
-      }
+		VirtualFile virtualFile = psiElement.getContainingFile().getVirtualFile();
+		if(virtualFile == null)
+		{
+			return null;
+		}
+		if(!virtualFile.isValid())
+		{
+			return null;
+		}
 
-      Document document = FileDocumentManager.getInstance().getDocument(virtualFile);
-      return new VcsSelection(document, textRange, actionName);
-  }
+		Document document = FileDocumentManager.getInstance().getDocument(virtualFile);
+		return new VcsSelection(document, textRange, actionName);
+	}
 }
