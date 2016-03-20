1b48b544798899f661576727261d91eaeacb5cbb
VISTALL <vistall.valeriy@gmail.com>
2016-03-20 13:18:30 +0300

    merge

---
+++ xml-impl/src/com/intellij/codeInsight/editorActions/moveLeftRight/XmlMoveLeftRightHandler.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.codeInsight.editorActions.moveLeftRight;
+
+import org.jetbrains.annotations.NotNull;
+import org.mustbe.consulo.RequiredReadAction;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.xml.XmlTag;
+
+public class XmlMoveLeftRightHandler implements MoveElementLeftRightHandler
+{
+	@RequiredReadAction
+	@NotNull
+	@Override
+	public PsiElement[] getMovableSubElements(@NotNull PsiElement element)
+	{
+		if(element instanceof XmlTag)
+		{
+			return ((XmlTag) element).getAttributes();
+		}
+		return PsiElement.EMPTY_ARRAY;
+	}
+}
