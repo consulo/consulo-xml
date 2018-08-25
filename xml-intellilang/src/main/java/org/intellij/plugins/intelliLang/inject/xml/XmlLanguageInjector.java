d8676c98d593ea8801dfb7b6611a0df14ed5930e
Valery Semenchuk <vistall.valeriy@gmail.com>
2018-08-25 10:28:43 +0300

    fix injection key

--- xml-intellilang/src/main/java/org/intellij/plugins/intelliLang/inject/xml/XmlLanguageInjector.java
+++ xml-intellilang/src/main/java/org/intellij/plugins/intelliLang/inject/xml/XmlLanguageInjector.java
@@ -16,6 +16,26 @@
 
 package org.intellij.plugins.intelliLang.inject.xml;
 
+import gnu.trove.THashMap;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.intellij.plugins.intelliLang.Configuration;
+import org.intellij.plugins.intelliLang.inject.InjectedLanguage;
+import org.intellij.plugins.intelliLang.inject.InjectorUtils;
+import org.intellij.plugins.intelliLang.inject.LanguageInjectionSupport;
+import org.intellij.plugins.intelliLang.inject.config.AbstractTagInjection;
+import org.intellij.plugins.intelliLang.inject.config.BaseInjection;
+import org.intellij.plugins.intelliLang.inject.config.InjectionPlace;
 import com.intellij.lang.Language;
 import com.intellij.lang.injection.MultiHostInjector;
 import com.intellij.lang.injection.MultiHostRegistrar;
@@ -29,22 +49,16 @@ import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtil;
-import com.intellij.psi.xml.*;
+import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.psi.xml.XmlElement;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.psi.xml.XmlText;
+import com.intellij.psi.xml.XmlToken;
+import com.intellij.psi.xml.XmlTokenType;
 import com.intellij.util.PairProcessor;
 import com.intellij.util.PatternValuesIndex;
-import gnu.trove.THashMap;
-import org.intellij.plugins.intelliLang.Configuration;
-import org.intellij.plugins.intelliLang.inject.InjectedLanguage;
-import org.intellij.plugins.intelliLang.inject.InjectorUtils;
-import org.intellij.plugins.intelliLang.inject.LanguageInjectionSupport;
-import org.intellij.plugins.intelliLang.inject.config.AbstractTagInjection;
-import org.intellij.plugins.intelliLang.inject.config.BaseInjection;
-import org.intellij.plugins.intelliLang.inject.config.InjectionPlace;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import java.util.*;
-import java.util.regex.Pattern;
+import consulo.psi.injection.impl.ProjectInjectionConfiguration;
 
 /**
  * This is the main part of the injection code. The component registers a language injector, the reference provider that
@@ -61,7 +75,7 @@ public final class XmlLanguageInjector implements MultiHostInjector {
   private volatile Trinity<Long, Pattern, Collection<String>> myXmlIndex;
   private final LanguageInjectionSupport mySupport;
 
-  public XmlLanguageInjector(Configuration configuration) {
+  public XmlLanguageInjector(ProjectInjectionConfiguration configuration) {
     myConfiguration = configuration;
     mySupport = InjectorUtils.findNotNullInjectionSupport(XmlLanguageInjectionSupport.XML_SUPPORT_ID);
   }
