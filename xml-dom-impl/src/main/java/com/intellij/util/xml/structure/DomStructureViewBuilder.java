6ac76aff99a9c65c3d637d89dbeae18a5b41b1aa
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-05-14 09:28:21 +0300

    compilation

--- xml-dom-impl/src/main/java/com/intellij/util/xml/structure/DomStructureViewBuilder.java
+++ xml-dom-impl/src/main/java/com/intellij/util/xml/structure/DomStructureViewBuilder.java
@@ -1,23 +1,7 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.util.xml.structure;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import com.intellij.ide.structureView.StructureView;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
@@ -27,7 +11,6 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.TextEditor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.AsyncResult;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlElement;
 import com.intellij.psi.xml.XmlFile;
@@ -36,6 +19,10 @@ import com.intellij.util.Function;
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.DomManager;
 import com.intellij.util.xml.DomService;
+import org.jetbrains.concurrency.Promise;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class DomStructureViewBuilder extends TreeBasedStructureViewBuilder
 {
@@ -56,20 +43,13 @@ public class DomStructureViewBuilder extends TreeBasedStructureViewBuilder
 	}
 
 	@Override
-	public boolean isRootNodeShown()
-	{
-		return true;
-	}
-
-	@Override
 	@Nonnull
 	public StructureView createStructureView(final FileEditor fileEditor, @Nonnull final Project project)
 	{
-		return new StructureViewComponent(fileEditor, createStructureViewModel(fileEditor instanceof TextEditor ? ((TextEditor) fileEditor)
-				.getEditor() : null), project, true)
+		return new StructureViewComponent(fileEditor, createStructureViewModel(fileEditor instanceof TextEditor ? ((TextEditor) fileEditor).getEditor() : null), project, true)
 		{
 			@Override
-			public AsyncResult<AbstractTreeNode> expandPathToElement(final Object element)
+			public Promise<AbstractTreeNode> expandPathToElement(final Object element)
 			{
 				if(element instanceof XmlElement && ((XmlElement) element).isValid())
 				{
