e8bab243595d195ebe82bef6e9bcadc8033628ad
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-05-17 11:17:16 +0300

    compilation

--- xml-dom-impl/src/main/java/com/intellij/util/xml/ui/EditorTextFieldControl.java
+++ xml-dom-impl/src/main/java/com/intellij/util/xml/ui/EditorTextFieldControl.java
@@ -17,11 +17,10 @@ package com.intellij.util.xml.ui;
 
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.Result;
 import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.command.CommandProcessor;
-import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.event.DocumentAdapter;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.event.DocumentListener;
@@ -35,8 +34,8 @@ import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.highlighting.DomElementAnnotationsManager;
 import com.intellij.util.xml.highlighting.DomElementProblemDescriptor;
 import com.intellij.util.xml.highlighting.DomElementsProblemsHolder;
-import javax.annotation.Nonnull;
 
+import javax.annotation.Nonnull;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -45,142 +44,180 @@ import java.util.List;
 /**
  * @author peter
  */
-public abstract class EditorTextFieldControl<T extends JComponent> extends BaseModifiableControl<T, String> {
-  private static final JTextField J_TEXT_FIELD = new JTextField() {
-    public void addNotify() {
-      throw new UnsupportedOperationException("Shouldn't be shown");
-    }
+public abstract class EditorTextFieldControl<T extends JComponent> extends BaseModifiableControl<T, String>
+{
+	private static final JTextField J_TEXT_FIELD = new JTextField()
+	{
+		public void addNotify()
+		{
+			throw new UnsupportedOperationException("Shouldn't be shown");
+		}
 
-    public void setVisible(boolean aFlag) {
-      throw new UnsupportedOperationException("Shouldn't be shown");
-    }
-  };
-  private final boolean myCommitOnEveryChange;
-  private final DocumentListener myListener = new DocumentAdapter() {
-    public void documentChanged(DocumentEvent e) {
-      setModified();
-      if (myCommitOnEveryChange) {
-        commit();
-      }
-    }
-  };
+		public void setVisible(boolean aFlag)
+		{
+			throw new UnsupportedOperationException("Shouldn't be shown");
+		}
+	};
+	private final boolean myCommitOnEveryChange;
+	private final DocumentListener myListener = new DocumentAdapter()
+	{
+		public void documentChanged(DocumentEvent e)
+		{
+			setModified();
+			if(myCommitOnEveryChange)
+			{
+				commit();
+			}
+		}
+	};
 
-  protected EditorTextFieldControl(final DomWrapper<String> domWrapper, final boolean commitOnEveryChange) {
-    super(domWrapper);
-    myCommitOnEveryChange = commitOnEveryChange;
-  }
+	protected EditorTextFieldControl(final DomWrapper<String> domWrapper, final boolean commitOnEveryChange)
+	{
+		super(domWrapper);
+		myCommitOnEveryChange = commitOnEveryChange;
+	}
 
 
-  protected EditorTextFieldControl(final DomWrapper<String> domWrapper) {
-    this(domWrapper, false);
-  }
+	protected EditorTextFieldControl(final DomWrapper<String> domWrapper)
+	{
+		this(domWrapper, false);
+	}
 
-  protected abstract EditorTextField getEditorTextField(@Nonnull T component);
+	protected abstract EditorTextField getEditorTextField(@Nonnull T component);
 
-  protected void doReset() {
-    final EditorTextField textField = getEditorTextField(getComponent());
-    textField.getDocument().removeDocumentListener(myListener);
-    super.doReset();
-    textField.getDocument().addDocumentListener(myListener);
-  }
+	protected void doReset()
+	{
+		final EditorTextField textField = getEditorTextField(getComponent());
+		textField.getDocument().removeDocumentListener(myListener);
+		super.doReset();
+		textField.getDocument().addDocumentListener(myListener);
+	}
 
-  protected JComponent getComponentToListenFocusLost(final T component) {
-    return getEditorTextField(getComponent());
-  }
+	protected JComponent getComponentToListenFocusLost(final T component)
+	{
+		return getEditorTextField(getComponent());
+	}
 
-  protected JComponent getHighlightedComponent(final T component) {
-    return J_TEXT_FIELD;
-  }
+	protected JComponent getHighlightedComponent(final T component)
+	{
+		return J_TEXT_FIELD;
+	}
 
-  protected T createMainComponent(T boundedComponent) {
-    final Project project = getProject();
-    boundedComponent = createMainComponent(boundedComponent, project);
+	protected T createMainComponent(T boundedComponent)
+	{
+		final Project project = getProject();
+		boundedComponent = createMainComponent(boundedComponent, project);
 
-    final EditorTextField editorTextField = getEditorTextField(boundedComponent);
-    editorTextField.setSupplementary(true);
-    editorTextField.getDocument().addDocumentListener(myListener);
-    return boundedComponent;
-  }
+		final EditorTextField editorTextField = getEditorTextField(boundedComponent);
+		editorTextField.setSupplementary(true);
+		editorTextField.getDocument().addDocumentListener(myListener);
+		return boundedComponent;
+	}
 
-  protected abstract T createMainComponent(T boundedComponent, Project project);
+	protected abstract T createMainComponent(T boundedComponent, Project project);
 
-  @Nonnull
-  protected String getValue() {
-    return getEditorTextField(getComponent()).getText();
-  }
+	@Nonnull
+	protected String getValue()
+	{
+		return getEditorTextField(getComponent()).getText();
+	}
 
-  protected void setValue(final String value) {
-    CommandProcessor.getInstance().runUndoTransparentAction(new Runnable() {
-      public void run() {
-        new WriteAction() {
-          protected void run(Result result) throws Throwable {
-            final T component = getComponent();
-            final Document document = getEditorTextField(component).getDocument();
-            document.replaceString(0, document.getTextLength(), value == null ? "" : value);
-          }
-        }.execute();
-      }
-    });
-  }
+	protected void setValue(final String value)
+	{
+		CommandProcessor.getInstance().runUndoTransparentAction(new Runnable()
+		{
+			public void run()
+			{
+				WriteAction.run(() -> {
+					final T component = getComponent();
+					final Document document = getEditorTextField(component).getDocument();
+					document.replaceString(0, document.getTextLength(), value == null ? "" : value);
+				});
+			}
+		});
+	}
 
-  protected void updateComponent() {
-    final DomElement domElement = getDomElement();
-    if (domElement == null || !domElement.isValid()) return;
+	protected void updateComponent()
+	{
+		final DomElement domElement = getDomElement();
+		if(domElement == null || !domElement.isValid())
+		{
+			return;
+		}
 
-    final EditorTextField textField = getEditorTextField(getComponent());
-    final Project project = getProject();
-    ApplicationManager.getApplication().invokeLater(new Runnable() {
-      public void run() {
-        if (!project.isOpen()) return;
-        if (!getDomWrapper().isValid()) return;
+		final EditorTextField textField = getEditorTextField(getComponent());
+		final Project project = getProject();
+		ApplicationManager.getApplication().invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				if(!project.isOpen())
+				{
+					return;
+				}
+				if(!getDomWrapper().isValid())
+				{
+					return;
+				}
 
-        final DomElement domElement = getDomElement();
-        if (domElement == null || !domElement.isValid()) return;
+				final DomElement domElement = getDomElement();
+				if(domElement == null || !domElement.isValid())
+				{
+					return;
+				}
 
-        final DomElementAnnotationsManager manager = DomElementAnnotationsManager.getInstance(project);
-        final DomElementsProblemsHolder holder = manager.getCachedProblemHolder(domElement);
-        final List<DomElementProblemDescriptor> errorProblems = holder.getProblems(domElement);
-        final List<DomElementProblemDescriptor> warningProblems = new ArrayList<DomElementProblemDescriptor>(holder.getProblems(domElement, true, HighlightSeverity.WARNING));
-        warningProblems.removeAll(errorProblems);
+				final DomElementAnnotationsManager manager = DomElementAnnotationsManager.getInstance(project);
+				final DomElementsProblemsHolder holder = manager.getCachedProblemHolder(domElement);
+				final List<DomElementProblemDescriptor> errorProblems = holder.getProblems(domElement);
+				final List<DomElementProblemDescriptor> warningProblems = new ArrayList<DomElementProblemDescriptor>(holder.getProblems(domElement, true, HighlightSeverity.WARNING));
+				warningProblems.removeAll(errorProblems);
 
-        Color background = getDefaultBackground();
-        if (errorProblems.size() > 0 && textField.getText().trim().length() == 0) {
-          background = getErrorBackground();
-        }
-        else if (warningProblems.size() > 0) {
-          background = getWarningBackground();
-        }
+				Color background = getDefaultBackground();
+				if(errorProblems.size() > 0 && textField.getText().trim().length() == 0)
+				{
+					background = getErrorBackground();
+				}
+				else if(warningProblems.size() > 0)
+				{
+					background = getWarningBackground();
+				}
 
-        final Editor editor = textField.getEditor();
-        if (editor != null) {
-          final MarkupModel markupModel = editor.getMarkupModel();
-          markupModel.removeAllHighlighters();
-          if (!errorProblems.isEmpty() && editor.getDocument().getLineCount() > 0) {
-            final TextAttributes attributes = SimpleTextAttributes.ERROR_ATTRIBUTES.toTextAttributes();
-            attributes.setEffectType(EffectType.WAVE_UNDERSCORE);
-            attributes.setEffectColor(attributes.getForegroundColor());
-            markupModel.addLineHighlighter(0, 0, attributes);
-            editor.getContentComponent().setToolTipText(errorProblems.get(0).getDescriptionTemplate());
-          }
-        }
+				final Editor editor = textField.getEditor();
+				if(editor != null)
+				{
+					final MarkupModel markupModel = editor.getMarkupModel();
+					markupModel.removeAllHighlighters();
+					if(!errorProblems.isEmpty() && editor.getDocument().getLineCount() > 0)
+					{
+						final TextAttributes attributes = SimpleTextAttributes.ERROR_ATTRIBUTES.toTextAttributes();
+						attributes.setEffectType(EffectType.WAVE_UNDERSCORE);
+						attributes.setEffectColor(attributes.getForegroundColor());
+						markupModel.addLineHighlighter(0, 0, attributes);
+						editor.getContentComponent().setToolTipText(errorProblems.get(0).getDescriptionTemplate());
+					}
+				}
 
-        textField.setBackground(background);
-      }
-    });
+				textField.setBackground(background);
+			}
+		});
 
-  }
+	}
 
-  public boolean canNavigate(final DomElement element) {
-    return getDomElement().equals(element);
-  }
+	public boolean canNavigate(final DomElement element)
+	{
+		return getDomElement().equals(element);
+	}
 
-  public void navigate(final DomElement element) {
-    final EditorTextField field = getEditorTextField(getComponent());
-    SwingUtilities.invokeLater(new Runnable() {
-      public void run() {
-        field.requestFocus();
-        field.selectAll();
-      }
-    });
-  }
+	public void navigate(final DomElement element)
+	{
+		final EditorTextField field = getEditorTextField(getComponent());
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				field.requestFocus();
+				field.selectAll();
+			}
+		});
+	}
 }
