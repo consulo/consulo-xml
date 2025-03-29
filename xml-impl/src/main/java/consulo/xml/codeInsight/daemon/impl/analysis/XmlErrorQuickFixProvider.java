9835e1f99ad6b7f979ec435445946a9b918bc3e4
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-03-29 09:47:33 +0400

    remove hardcoded localization check for specific error

--- xml-impl/src/main/java/consulo/xml/codeInsight/daemon/impl/analysis/XmlErrorQuickFixProvider.java
+++ xml-impl/src/main/java/consulo/xml/codeInsight/daemon/impl/analysis/XmlErrorQuickFixProvider.java
@@ -21,11 +21,18 @@ import consulo.language.editor.intention.ErrorQuickFixProvider;
 import consulo.language.editor.rawHighlight.HighlightInfo;
 import consulo.language.psi.PsiErrorElement;
 import consulo.language.psi.util.PsiTreeUtil;
+import consulo.localize.LocalizeKey;
+import consulo.localize.LocalizeValue;
+import consulo.util.lang.lazy.LazyValue;
 import consulo.xml.impl.localize.XmlErrorLocalize;
 import consulo.xml.psi.xml.XmlTag;
 
 @ExtensionImpl
 public class XmlErrorQuickFixProvider implements ErrorQuickFixProvider {
+    private static final LazyValue<LocalizeKey> unescapedAmpersandKey = LazyValue.notNull(() -> {
+        return XmlErrorLocalize.unescapedAmpersandOrNonterminatedCharacterEntityReference().getKey().get();
+    });
+
     @Override
     @RequiredReadAction
     public void registerErrorQuickFix(final PsiErrorElement element, final HighlightInfo.Builder builder) {
@@ -35,8 +42,9 @@ public class XmlErrorQuickFixProvider implements ErrorQuickFixProvider {
     }
 
     private static void registerXmlErrorQuickFix(final PsiErrorElement element, final HighlightInfo.Builder builder) {
-        final String text = element.getErrorDescription();
-        if (text != null && text.startsWith(XmlErrorLocalize.unescapedAmpersand().get())) {
+        LocalizeValue errorText = element.getErrorDescriptionValue();
+
+        if (errorText.getKey().isPresent() && errorText.getKey().get().equals(unescapedAmpersandKey.get())) {
             builder.registerFix(new UnescapeAction(element), null, null, null, null);
         }
     }
