f73f0117ff9071fd13a1b2cafbd37b9b30ff5e4d
UNV <nikolay@yurchenko.su>
2025-04-12 20:52:00 +0300

    Refactoring of SchemaDefinitionsSearch.

--- xml-impl/src/main/java/com/intellij/xml/impl/schema/SchemaDefinitionsSearch.java
+++ xml-impl/src/main/java/com/intellij/xml/impl/schema/SchemaDefinitionsSearch.java
@@ -19,10 +19,9 @@ import com.intellij.xml.index.SchemaTypeInfo;
 import com.intellij.xml.index.SchemaTypeInheritanceIndex;
 import com.intellij.xml.index.XmlNamespaceIndex;
 import com.intellij.xml.util.XmlUtil;
+import consulo.annotation.access.RequiredReadAction;
 import consulo.annotation.component.ExtensionImpl;
-import consulo.application.ApplicationManager;
-import consulo.application.util.function.Computable;
-import consulo.application.util.function.Processor;
+import consulo.application.Application;
 import consulo.language.psi.PsiElement;
 import consulo.language.psi.resolve.PsiElementProcessor;
 import consulo.language.psi.search.DefinitionsScopedSearch;
@@ -40,92 +39,77 @@ import jakarta.annotation.Nonnull;
 
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 /**
- * User: Irina.Chernushina
- * Date: 7/5/12
- * Time: 5:34 PM
+ * @author Irina.Chernushina
+ * @since 2021-07-05
  */
 @ExtensionImpl
 public class SchemaDefinitionsSearch implements DefinitionsScopedSearchExecutor {
     @Override
+    @RequiredReadAction
     public boolean execute(
-        @Nonnull final DefinitionsScopedSearch.SearchParameters parameters,
-        @Nonnull final Processor<? super PsiElement> consumer
+        @Nonnull DefinitionsScopedSearch.SearchParameters parameters,
+        @Nonnull Predicate<? super PsiElement> consumer
     ) {
         PsiElement queryParameters = parameters.getElement();
-        if (queryParameters instanceof XmlTagImpl) {
-            final XmlTagImpl xml = (XmlTagImpl)queryParameters;
-            if (isTypeElement(xml)) {
-                final Collection<SchemaTypeInfo> infos =
-                    ApplicationManager.getApplication().runReadAction(new Computable<Collection<SchemaTypeInfo>>() {
-                        @Override
-                        public Collection<SchemaTypeInfo> compute() {
-                            return gatherInheritors(xml);
+        if (queryParameters instanceof XmlTagImpl xml && isTypeElement(xml)) {
+            Application app = Application.get();
+            Collection<SchemaTypeInfo> infos =
+                app.runReadAction((Supplier<Collection<SchemaTypeInfo>>)() -> gatherInheritors(xml));
+
+            if (infos != null && !infos.isEmpty()) {
+                XmlFile file = XmlUtil.getContainingFile(xml);
+                Project project = file.getProject();
+                Module module = queryParameters.getModule();
+                //if (module == null) return false;
+
+                VirtualFile vf = file.getVirtualFile();
+                String thisNs = XmlNamespaceIndex.getNamespace(vf, project, file);
+                thisNs = thisNs == null ? getDefaultNs(file) : thisNs;
+                // so thisNs can be null
+                if (thisNs == null) {
+                    return false;
+                }
+                List<SchemaTypeInfo> infosLst = new ArrayList<>(infos);
+                Collections.sort(infosLst);
+                Map<String, Set<XmlFile>> nsMap = new HashMap<>();
+                for (SchemaTypeInfo info : infosLst) {
+                    Set<XmlFile> targetFiles = nsMap.get(info.getNamespaceUri());
+                    if (targetFiles == null) {
+                        targetFiles = new HashSet<>();
+                        if (Comparing.equal(info.getNamespaceUri(), thisNs)) {
+                            targetFiles.add(file);
                         }
-                    });
-
-                if (infos != null && !infos.isEmpty()) {
-                    XmlFile file = XmlUtil.getContainingFile(xml);
-                    final Project project = file.getProject();
-                    final Module module = queryParameters.getModule();
-                    //if (module == null) return false;
-
-                    final VirtualFile vf = file.getVirtualFile();
-                    String thisNs = XmlNamespaceIndex.getNamespace(vf, project, file);
-                    thisNs = thisNs == null ? getDefaultNs(file) : thisNs;
-                    // so thisNs can be null
-                    if (thisNs == null) {
-                        return false;
+                        Collection<XmlFile> files = app.runReadAction(
+                            (Supplier<Collection<XmlFile>>)() -> XmlUtil.findNSFilesByURI(info.getNamespaceUri(), project, module)
+                        );
+                        if (files != null) {
+                            targetFiles.addAll(files);
+                        }
+                        nsMap.put(info.getNamespaceUri(), targetFiles);
                     }
-                    final ArrayList<SchemaTypeInfo> infosLst = new ArrayList<SchemaTypeInfo>(infos);
-                    Collections.sort(infosLst);
-                    final Map<String, Set<XmlFile>> nsMap = new HashMap<String, Set<XmlFile>>();
-                    for (final SchemaTypeInfo info : infosLst) {
-                        Set<XmlFile> targetFiles = nsMap.get(info.getNamespaceUri());
-                        if (targetFiles == null) {
-                            targetFiles = new HashSet<>();
-                            if (Comparing.equal(info.getNamespaceUri(), thisNs)) {
-                                targetFiles.add(file);
-                            }
-                            final Collection<XmlFile> files =
-                                ApplicationManager.getApplication().runReadAction(new Computable<Collection<XmlFile>>() {
-                                    @Override
-                                    public Collection<XmlFile> compute() {
-                                        return XmlUtil.findNSFilesByURI(info.getNamespaceUri(), project, module);
-                                    }
-                                });
-                            if (files != null) {
-                                targetFiles.addAll(files);
-                            }
-                            nsMap.put(info.getNamespaceUri(), targetFiles);
-                        }
-                        if (!targetFiles.isEmpty()) {
-                            for (final XmlFile targetFile : targetFiles) {
-                                ApplicationManager.getApplication().runReadAction(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        final String prefixByURI = XmlUtil.findNamespacePrefixByURI(targetFile, info.getNamespaceUri());
-                                        if (prefixByURI == null) {
-                                            return;
+                    if (!targetFiles.isEmpty()) {
+                        for (XmlFile targetFile : targetFiles) {
+                            app.runReadAction(() -> {
+                                String prefixByURI = XmlUtil.findNamespacePrefixByURI(targetFile, info.getNamespaceUri());
+                                if (prefixByURI == null) {
+                                    return;
+                                }
+                                PsiElementProcessor processor = element -> {
+                                    if (element instanceof XmlTagImpl tag) {
+                                        if (isCertainTypeElement(tag, info.getTagName(), prefixByURI)
+                                            || isElementWithEmbeddedType(tag, info.getTagName(), prefixByURI)) {
+                                            consumer.test(element);
+                                            return false;
                                         }
-                                        final PsiElementProcessor processor = new PsiElementProcessor() {
-                                            @Override
-                                            public boolean execute(@Nonnull PsiElement element) {
-                                                if (element instanceof XmlTagImpl tag) {
-                                                    if (isCertainTypeElement(tag, info.getTagName(), prefixByURI)
-                                                        || isElementWithEmbeddedType(tag, info.getTagName(), prefixByURI)) {
-                                                        consumer.process(element);
-                                                        return false;
-                                                    }
-                                                }
-                                                return true;
-                                            }
-                                        };
-                                        XmlUtil.processXmlElements(targetFile, processor, true);
                                     }
-                                });
-                            }
+                                    return true;
+                                };
+                                XmlUtil.processXmlElements(targetFile, processor, true);
+                            });
                         }
                     }
                 }
@@ -135,11 +119,11 @@ public class SchemaDefinitionsSearch implements DefinitionsScopedSearchExecutor 
     }
 
     public static boolean isElementWithSomeEmbeddedType(XmlTagImpl xml) {
-        final String localName = xml.getLocalName();
+        String localName = xml.getLocalName();
         if (!(XmlUtil.XML_SCHEMA_URI.equals(xml.getNamespace()) && "element".equals(localName))) {
             return false;
         }
-        final XmlTag[] tags = xml.getSubTags();
+        XmlTag[] tags = xml.getSubTags();
         for (XmlTag tag : tags) {
             if (isTypeElement((XmlTagImpl)tag)) {
                 return true;
@@ -148,21 +132,21 @@ public class SchemaDefinitionsSearch implements DefinitionsScopedSearchExecutor 
         return false;
     }
 
-    public static boolean isElementWithEmbeddedType(XmlTagImpl xml, final String typeName, final String typeNsPrefix) {
-        final String localName = xml.getLocalName();
+    public static boolean isElementWithEmbeddedType(XmlTagImpl xml, String typeName, String typeNsPrefix) {
+        String localName = xml.getLocalName();
         if (!(XmlUtil.XML_SCHEMA_URI.equals(xml.getNamespace()) && "element".equals(localName))) {
             return false;
         }
-        final XmlAttribute nameAttr = getNameAttr(xml);
+        XmlAttribute nameAttr = getNameAttr(xml);
         if (nameAttr == null || nameAttr.getValue() == null) {
             return false;
         }
-        final String localTypeName = XmlUtil.findLocalNameByQualifiedName(nameAttr.getValue());
-        final String prefix = XmlUtil.findPrefixByQualifiedName(nameAttr.getValue());
+        String localTypeName = XmlUtil.findLocalNameByQualifiedName(nameAttr.getValue());
+        String prefix = XmlUtil.findPrefixByQualifiedName(nameAttr.getValue());
         if (!typeName.equals(localTypeName) || !typeNsPrefix.equals(prefix)) {
             return false;
         }
-        final XmlTag[] tags = xml.getSubTags();
+        XmlTag[] tags = xml.getSubTags();
         for (XmlTag tag : tags) {
             if (isTypeElement((XmlTagImpl)tag)) {
                 return true;
@@ -171,24 +155,24 @@ public class SchemaDefinitionsSearch implements DefinitionsScopedSearchExecutor 
         return false;
     }
 
-    private boolean isCertainTypeElement(XmlTagImpl xml, final String typeName, final String nsPrefix) {
+    private boolean isCertainTypeElement(XmlTagImpl xml, String typeName, String nsPrefix) {
         if (!isTypeElement(xml)) {
             return false;
         }
-        final XmlAttribute name = getNameAttr(xml);
+        XmlAttribute name = getNameAttr(xml);
         if (name == null) {
             return false;
         }
-        final String value = name.getValue();
+        String value = name.getValue();
         if (value == null) {
             return false;
         }
-        final String localName = XmlUtil.findLocalNameByQualifiedName(value);
+        String localName = XmlUtil.findLocalNameByQualifiedName(value);
         return typeName.equals(localName) && nsPrefix.equals(XmlUtil.findPrefixByQualifiedName(value));
     }
 
     public static boolean isTypeElement(XmlTagImpl xml) {
-        final String localName = xml.getLocalName();
+        String localName = xml.getLocalName();
         return XmlUtil.XML_SCHEMA_URI.equals(xml.getNamespace()) && ("complexType".equals(localName) || "simpleType".equals(localName));
     }
 
@@ -198,21 +182,21 @@ public class SchemaDefinitionsSearch implements DefinitionsScopedSearchExecutor 
             return null;
         }
         String localName = name.getValue();
-        final boolean hasPrefix = localName.contains(":");
+        boolean hasPrefix = localName.contains(":");
         localName = hasPrefix ? localName.substring(localName.indexOf(':') + 1) : localName;
-        final String nsPrefix = hasPrefix ? name.getValue().substring(0, name.getValue().indexOf(':')) : null;
+        String nsPrefix = hasPrefix ? name.getValue().substring(0, name.getValue().indexOf(':')) : null;
 
-        final XmlFile file = XmlUtil.getContainingFile(xml);
+        XmlFile file = XmlUtil.getContainingFile(xml);
         if (file == null) {
             return null;
         }
-        final Project project = file.getProject();
+        Project project = file.getProject();
         if (project == null) {
             return null;
         }
 
-        final Set<SchemaTypeInfo> result = new HashSet<>();
-        final ArrayDeque<SchemaTypeInfo> queue = new ArrayDeque<>();
+        Set<SchemaTypeInfo> result = new HashSet<>();
+        ArrayDeque<SchemaTypeInfo> queue = new ArrayDeque<>();
 
         String nsUri;
         if (!hasPrefix) {
@@ -227,11 +211,11 @@ public class SchemaDefinitionsSearch implements DefinitionsScopedSearchExecutor 
 
         queue.add(new SchemaTypeInfo(localName, true, nsUri));
 
-        final BiFunction<String, String, List<Set<SchemaTypeInfo>>> worker =
+        BiFunction<String, String, List<Set<SchemaTypeInfo>>> worker =
             SchemaTypeInheritanceIndex.getWorker(project, file.getContainingFile().getVirtualFile());
         while (!queue.isEmpty()) {
-            final SchemaTypeInfo info = queue.removeFirst();
-            final List<Set<SchemaTypeInfo>> childrenOfType = worker.apply(info.getNamespaceUri(), info.getTagName());
+            SchemaTypeInfo info = queue.removeFirst();
+            List<Set<SchemaTypeInfo>> childrenOfType = worker.apply(info.getNamespaceUri(), info.getTagName());
             for (Set<SchemaTypeInfo> infos : childrenOfType) {
                 for (SchemaTypeInfo typeInfo : infos) {
                     if (typeInfo.isIsTypeName()) {
@@ -250,17 +234,14 @@ public class SchemaDefinitionsSearch implements DefinitionsScopedSearchExecutor 
         return name;
     }
 
-    private String getDefaultNs(final XmlFile file) {
-        return ApplicationManager.getApplication().runReadAction(new Computable<String>() {
-            @Override
-            public String compute() {
-                String nsUri;
-                final XmlTag tag = file.getDocument().getRootTag();
-                XmlAttribute xmlns = tag.getAttribute("xmlns", XmlUtil.XML_SCHEMA_URI);
-                xmlns = xmlns == null ? tag.getAttribute("xmlns") : xmlns;
-                nsUri = xmlns == null ? null : xmlns.getValue();
-                return nsUri;
-            }
+    private String getDefaultNs(XmlFile file) {
+        return Application.get().runReadAction((Supplier<String>)() -> {
+            String nsUri;
+            XmlTag tag = file.getDocument().getRootTag();
+            XmlAttribute xmlns = tag.getAttribute("xmlns", XmlUtil.XML_SCHEMA_URI);
+            xmlns = xmlns == null ? tag.getAttribute("xmlns") : xmlns;
+            nsUri = xmlns == null ? null : xmlns.getValue();
+            return nsUri;
         });
     }
 }
