b73b283d4d97e94f78ad2a37dcfa125a2e2f722e
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-06-21 18:44:51 +0400

    fix using impl class

--- xml-impl/src/main/java/module-info.java
+++ xml-impl/src/main/java/module-info.java
@@ -172,4 +172,6 @@ open module com.intellij.xml
     exports org.intellij.plugins.relaxNG.xml.dom;
     exports org.intellij.plugins.relaxNG.xml.dom.impl;
     exports org.intellij.plugins.relaxNG.xml.dom.names;
+
+    //opens consulo.xml.psi.impl.source.xml to consulo.util.concurrent;
 }
