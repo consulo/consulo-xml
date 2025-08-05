dec8fe9b87e17c96033ec1a4f6b26f298c0255b9
UNV <nikolay@yurchenko.su>
2025-08-05 20:18:26 +0300

    Annotating EscapeEntitiesAction with @ActionImpl. Refactoring and localizing.

--- xml-impl/src/main/java/com/intellij/xml/actions/EscapeEntitiesAction.java
+++ xml-impl/src/main/java/com/intellij/xml/actions/EscapeEntitiesAction.java
@@ -3,6 +3,11 @@ package com.intellij.xml.actions;
 
 import com.intellij.xml.Html5SchemaProvider;
 import com.intellij.xml.util.XmlUtil;
+import consulo.annotation.access.RequiredReadAction;
+import consulo.annotation.component.ActionImpl;
+import consulo.annotation.component.ActionParentRef;
+import consulo.annotation.component.ActionRef;
+import consulo.annotation.component.ActionRefAnchor;
 import consulo.codeEditor.Editor;
 import consulo.document.Document;
 import consulo.document.util.TextRange;
@@ -13,27 +18,37 @@ import consulo.language.psi.PsiElement;
 import consulo.language.psi.PsiErrorElement;
 import consulo.language.psi.PsiFile;
 import consulo.project.Project;
+import consulo.ui.annotation.RequiredUIAccess;
 import consulo.util.collection.primitive.ints.IntMaps;
 import consulo.util.collection.primitive.ints.IntObjectMap;
+import consulo.xml.impl.localize.XmlLocalize;
 import consulo.xml.psi.xml.XmlAttribute;
 import consulo.xml.psi.xml.XmlEntityDecl;
 import consulo.xml.psi.xml.XmlFile;
 import consulo.xml.psi.xml.XmlTokenType;
-
 import jakarta.annotation.Nonnull;
 
 /**
  * @author Dennis.Ushakov
  */
+@ActionImpl(
+    id = "EscapeEntities",
+    parents = @ActionParentRef(value = @ActionRef(id = "EditMenu"), anchor = ActionRefAnchor.LAST)
+)
 public class EscapeEntitiesAction extends BaseCodeInsightAction implements CodeInsightActionHandler {
+    public EscapeEntitiesAction() {
+        super(XmlLocalize.actionEscapeEntitiesText(), XmlLocalize.actionEscapeEntitiesDescription());
+    }
+
+    @RequiredReadAction
     private static String escape(XmlFile file, IntObjectMap<String> map, String text, int start) {
-        final StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
-            final PsiElement element = file.findElementAt(start + i);
+            PsiElement element = file.findElementAt(start + i);
             if (element != null && isCharacterElement(element)) {
                 if (c == '<' || c == '>' || c == '&' || c == '"' || c == '\'' || c > 0x7f) {
-                    final String escape = map.get(c);
+                    String escape = map.get(c);
                     if (escape != null) {
                         result.append("&").append(escape).append(";");
                         continue;
@@ -47,25 +62,30 @@ public class EscapeEntitiesAction extends BaseCodeInsightAction implements CodeI
 
     @Nonnull
     private static IntObjectMap<String> computeMap(XmlFile xmlFile) {
-        final XmlFile file = XmlUtil.findXmlFile(xmlFile, Html5SchemaProvider.getCharsDtdLocation());
+        XmlFile file = XmlUtil.findXmlFile(xmlFile, Html5SchemaProvider.getCharsDtdLocation());
         assert file != null;
 
-        final IntObjectMap<String> result = IntMaps.newIntObjectHashMap();
-        XmlUtil.processXmlElements(file, element -> {
-            if (element instanceof XmlEntityDecl) {
-                final String value = ((XmlEntityDecl)element).getValueElement().getValue();
-                final int key = Integer.parseInt(value.substring(2, value.length() - 1));
-                if (!result.containsKey(key)) {
-                    result.put(key, ((XmlEntityDecl)element).getName());
+        IntObjectMap<String> result = IntMaps.newIntObjectHashMap();
+        XmlUtil.processXmlElements(
+            file,
+            element -> {
+                if (element instanceof XmlEntityDecl xmlEntityDecl) {
+                    String value = xmlEntityDecl.getValueElement().getValue();
+                    int key = Integer.parseInt(value.substring(2, value.length() - 1));
+                    if (!result.containsKey(key)) {
+                        result.put(key, xmlEntityDecl.getName());
+                    }
                 }
-            }
-            return true;
-        }, true);
+                return true;
+            },
+            true
+        );
         return result;
     }
 
+    @RequiredReadAction
     private static boolean isCharacterElement(PsiElement element) {
-        final IElementType type = element.getNode().getElementType();
+        IElementType type = element.getNode().getElementType();
         if (type == XmlTokenType.XML_DATA_CHARACTERS) {
             return true;
         }
@@ -95,17 +115,18 @@ public class EscapeEntitiesAction extends BaseCodeInsightAction implements CodeI
     }
 
     @Override
+    @RequiredUIAccess
     public void invoke(@Nonnull Project project, @Nonnull Editor editor, @Nonnull PsiFile file) {
         int[] starts = editor.getSelectionModel().getBlockSelectionStarts();
         int[] ends = editor.getSelectionModel().getBlockSelectionEnds();
-        final Document document = editor.getDocument();
-        XmlFile xmlFile = (XmlFile)file;
+        Document document = editor.getDocument();
+        XmlFile xmlFile = (XmlFile) file;
         IntObjectMap<String> map = computeMap(xmlFile);
         for (int i = starts.length - 1; i >= 0; i--) {
-            final int start = starts[i];
-            final int end = ends[i];
+            int start = starts[i];
+            int end = ends[i];
             String oldText = document.getText(new TextRange(start, end));
-            final String newText = escape(xmlFile, map, oldText, start);
+            String newText = escape(xmlFile, map, oldText, start);
             if (!oldText.equals(newText)) {
                 document.replaceString(start, end, newText);
             }
